dff63b8389b37d8be8688d019e25d9862f64bd96
Nishikata <70297729+NishikataK@users.noreply.github.com>
2020-11-05 19:43:23 +0530

    Just a small change

---
+++ p.java
@@ -0,0 +1,21 @@
+well with the one
+who is the shooter
+this might would
+have never happened
+right?wrong, think
+about yourself as
+a shooter... would u
+listen to someone
+apologizing or shoot
+the hell out of
+him/her? well this is
+too good to be true
+so this is my word
+to all of ya, just
+behave well with
+everyone, dont bully
+dont harrass and 
+dont terrorize or
+uk you would be the
+one to blame for the
+attack...!
